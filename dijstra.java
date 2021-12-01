d08a58955e11b26d228bf70aa95cec24bdcc4055
Xunfengwang <xunfengwang@gmail.com>
2021-12-01 16:07:04 +0800

    update dij

--- dijstra.java
+++ dijstra.java
@@ -23,7 +23,6 @@ public class dijstra
         finalpath[0]=0;
         int test[]=new int[6];
         test[0]=0;
-        test[1]=1;
         for(int j=1;j<finalpath.length;j++)
         {
             finalpath[j]=999;
@@ -55,7 +54,6 @@ public class dijstra
                                     finalpath[k]=graphic[i][k]+finalpath[i];
                                     path[k]=i;
                                 }
-                                //print(finalpath);
                             }
                         }
                     }
@@ -73,20 +71,23 @@ public class dijstra
                 }
             }
         }
+        System.out.println("0 to any point mini cost:");
         print(finalpath);
     }
     public static void print(int[]dist)
     {
         for(int i=0;i<dist.length;i++)
+        { 
+            System.out.print(i+" point-cost");
+            System.out.print(" ");   
+        }
+        System.out.println("");
+        for(int i=0;i<dist.length;i++)
         {
             
-            System.out.print(dist[i]);
+            System.out.print("           "+dist[i]);
             System.out.print(" ");
         }
         System.out.println("");
-    }
-    public static void cal(int[]lastpoint,int[]dist)
-    {
-        int []arr=new int [dist.length];
     }
 }
