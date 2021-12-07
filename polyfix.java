6a9e3b54af8d5e9509b1feb4e454d74d11e754e6
Xunfengwang <xunfengwang@gmail.com>
2021-12-07 20:40:43 +0800

    poly almost complete

--- polyfix.java
+++ polyfix.java
@@ -8,36 +8,36 @@ class node {
 }
 public class polyfix {
 	static Scanner key = new Scanner(System.in);
-	public static node current, prev, next, ptr, head, tmp, head1, current1, head2, current2;
-    
+	public static node current, prev, next, ptr, head, tmp, head1, current1, head2, current2,tail,tail1,tail2;
+    public static int c=0;
+    public static int c1=0;
 	public static void main(String[] args) 
     { 
-        head=new node(); head.link=null;
-	    head1=new node();head1.link=null;
-        head2=new node();head2.link=null;
+        head=new node(); tail=new node(); head.link=tail; tail.link=null;
+	    head1=new node();tail1=new node();head1.link=tail1; tail1.link=null;
+        head2=new node();tail2=new node();head2.link=tail2; tail2.link=null;
         while(true)
         {
             System.out.println("1 for insert list one end,2 for insert list two end,3 show,4 show2,5 combine");
             int num=key.nextInt();
             if(num==1)
             {
-                listinsert(head,1);
+                listinsert(head,1,tail);
+                c++;
             }
             else if(num==2)
             {
-                listinsert(head1,2);
+                listinsert(head1,2,tail1);
+                c1++;
             }
             else if(num==3)
             {
-                showlist(head,1);
-            }
-            else if(num==4)
-            {
-                showlist(head1,2);
+                showlist(head,1,tail);
+                showlist(head1,2,tail1);
             }
             else if(num==5)
             {
-                combin(head,head1,head2);
+                combin(head,head1,head2,tail,tail1,tail2);
             }
             else
             {
@@ -45,7 +45,7 @@ public class polyfix {
             }
         }
     }
-    public static void listinsert(node innode,int a)
+    public static void listinsert(node innode,int a,node end)
     {
 
         while(a>0)
@@ -53,27 +53,27 @@ public class polyfix {
             if(a==1)
             {
                 current=head;
+                end=tail;
                 a=a-10;
             }
             else
             {
                 current=head1;
+                end=tail1;
                 a=a-10;
             }
         }
         if(current.coef==0&&current.exp==0)
         {
             
-            System.out.println("insert coef,exp");
+            System.out.println("this is empty insert coef,exp");
             current.coef=key.nextInt();
             current.exp=key.nextInt();
-            current.link=null;
         }
         else
         {
-            while(current.link!=null)
+            while(current.link!=end)
             {
-                prev=current;
                 current=current.link;
             }
             ptr=new node();
@@ -82,49 +82,59 @@ public class polyfix {
             int e=key.nextInt();
             ptr.coef=c;
             ptr.exp=e;
-            ptr.link=current.link;
+            ptr.link=end;
             current.link=ptr;
             
         }
     }
-    public static void showlist(node listnode,int a)
+    public static void showlist(node listnode,int a,node end)
     {
         while(a>0)
         {
             if(a==1)
             {
                 current=head;
+                end=tail;
                 a=a-10;
             }
             else if(a==2)
             {
                 current=head1;
+                end=tail1;
                 a=a-10;
             }
             else if(a==3)
             {
                 current=head2;
+                end=tail2;
                 a=a-10;
             }
         }
-        if(current.exp==0&&current.coef==0)
+        if(current.link==end&&current.coef==0&&current.exp==0)
         {
             System.out.println("empty");
         }
-            while(current.link!=null)
+        while(current!=end)
+        {
+            if(current.link!=tail&&current!=end)
             {
-                System.out.println(current.coef+"x"+current.exp+"^");
-                if(current.link!=null)
-                {
-                    current=current.link;
-                }
+                System.out.print(current.coef+"x"+current.exp+"^"+"+");
+                current=current.link;
             }
-            System.out.println(current.coef+"x"+current.exp+"^"+"1");
+            if(current.link==tail)
+            {
+                System.out.print(current.coef+"x"+current.exp+"^");
+                current=current.link;
+            }
+            if(current==end)
+            {
+                System.out.println("");
+            }
+        }
         
     }
-    public static void combin(node head,node head1,node head2)
+    public static void combin(node head,node head1,node head2,node tail,node tail1,node tail2)
     {
-        //head2=new node();
         current2=head2;
         current=head;
         current1=head1;
@@ -132,65 +142,36 @@ public class polyfix {
         {
             current2.exp=current1.exp*current.exp;
             current2.coef=current1.coef*current.coef;
-            //System.out.println(current2.coef+","+current2.exp);
-            showlist(head2,3);
         }
         else{
-            while(current.link!=null)
+            for(int i=1;i<=c;i++)
             {
-                while(current1.link!=null)
+                current1=head1;
+                for(int k=1;k<=c1;k++)
                 {
                     if(current2.exp==0&&current2.coef==0)
-                    {
-                    System.out.println(current.coef+","+current.exp+","+current1.coef+","+current1.exp+"1");    
+                    {    
                     current2.exp=current1.exp*current.exp;
                     current2.coef=current1.coef*current.coef;
                     current1=current1.link;
                     }
                     else
                     {
-                        System.out.println(current.coef+","+current.exp+","+current1.coef+","+current1.exp+"2");
-                    System.out.println(current1.coef+","+current1.exp+" test2");
-                    ptr=new node();
-                    ptr.exp=current1.exp*current.exp;
-                    ptr.coef=current1.coef*current.coef;
-                    System.out.println(ptr.coef+","+ptr.exp+" 3");
-                    ptr.link=current2.link;
-                    current2.link=ptr;
-                    if(current1.link!=null)
-                    {
+                        while(current2.link!=tail2)
+                        {
+                            current2=current2.link;
+                        }
+                        ptr=new node();
+                        ptr.exp=current1.exp*current.exp;
+                        ptr.coef=current1.coef*current.coef;
+                        ptr.link=tail2;
+                        current2.link=ptr;
                         current1=current1.link;
                     }
-                    }
                 }
-                
-                System.out.println(current.coef+","+current.exp+","+current1.coef+","+current1.exp+"3");
-                ptr=new node();
-                ptr.exp=current1.exp*current.exp;
-                ptr.coef=current1.coef*current.coef;
-                ptr.link=current2.link;
-                current2.link=ptr;
                 current=current.link;
-                current1=head1;
             }
-            System.out.println(current.coef+","+current.exp+","+current1.coef+","+current1.exp+"4");
-            ptr=new node();
-            ptr.exp=current1.exp*current.exp;
-            ptr.coef=current1.coef*current.coef;
-            ptr.link=current2.link;
-            current2.link=ptr;
-            /*if(current1.link!=null)
-            {
-                
-            current1=current1.link;
-            System.out.println(current.coef+","+current.exp+","+current1.coef+","+current1.exp+"5");
-            ptr=new node();
-            ptr.exp=current1.exp*current.exp;
-            ptr.coef=current1.coef*current.coef;
-            ptr.link=current2.link;
-            current2.link=ptr;
-            }*/
-            showlist(head2,3);
+            showlist(head2,3,tail2);
         }
     }
 }
