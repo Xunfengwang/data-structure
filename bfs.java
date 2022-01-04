9b87946a2abb700482354cd3e5c257297dbd15f4
Xunfengwang <xunfengwang@gmail.com>
2022-01-04 10:56:26 +0800

    no message

--- bfs.java
+++ bfs.java
@@ -1,94 +1,49 @@
 import java.util.Scanner;//先做走迷宮
 import java.util.ArrayList;
 public class bfs
-{private static int[][] maze =    //建立迷宫地图，0代表可走的路，1代表墙壁不可走
-    {
 {
-1,1,1,1,1,1,1,1,1,1},
-     {
-1,0,0,1,0,0,0,1,0,1},
-     {
-1,0,0,1,0,0,0,1,0,1},
-     {
-1,0,0,0,0,1,1,0,0,1},
-     {
-1,0,1,1,1,0,0,0,0,1},
-     {
-1,0,0,0,1,0,0,0,0,1},
-     {
-1,0,1,0,0,0,1,0,0,1},
-     {
-1,0,1,1,1,0,1,1,0,1},
-     {
-1,1,0,0,0,0,0,0,0,1},
-     {
-1,1,1,1,1,1,1,1,1,1}};
-private static ArrayList<Point> route = new ArrayList<Point>();  //走迷宫的路线
+    private int number = 9;  
+    private boolean[] flag;  
+    private String[] vertexs = { "A", "B", "C", "D", "E", "F", "G", "H", "I" };  
+    private int[][] edges = {   
+            { 0, 1, 0, 0, 0, 1, 1, 0, 0 }, 
+            { 1, 0, 1, 0, 0, 0, 1, 0, 1 }, 
+            { 0, 1, 0, 1, 0, 0, 0, 0, 1 },  
+            { 0, 0, 1, 0, 1, 0, 1, 1, 1 }, 
+            { 0, 0, 0, 1, 0, 1, 0, 1, 0 }, 
+            { 1, 0, 0, 0, 1, 0, 1, 0, 0 },  
+            { 0, 1, 0, 1, 0, 1, 0, 1, 0 }, 
+            { 0, 0, 0, 1, 1, 0, 1, 0, 0 }, 
+            { 0, 1, 1, 1, 0, 0, 0, 0, 0 }   
+            };
 
-//打印输出迷宫地图
-public static void main(String[] args) {
-
-for (int i = 0; i < maze.length; i++) {
-
-    for (int j = 0; j < maze.length; j++) {
-
-        System.out.print(maze[i][j]+"  ");
-    }
-    System.out.println();
-}
-        /*int [][]a={
-            {0,1,1,0,0,0,0,0},
-            {1,0,0,1,1,0,0,0},
-            {1,0,0,0,0,1,1,0},
-            {0,1,0,0,0,0,0,1},
-            {0,1,0,0,0,0,0,1},
-            {0,0,1,0,0,0,0,1},
-            {0,0,1,0,0,0,0,1},
-            {0,0,0,1,1,1,1,0}
-        };
-        findconnect(a);*/
-    }
-    private static boolean isWalk(int[][] maze,Point currentPoint){
-    
-        int x = currentPoint.x;
-        int y = currentPoint.y;
-        //往右走
-        if (maze[y][x+1]!=1 && maze[y][x+1]!=-1){
-       
-            route.add(new Point(x+1,y));
-            return true;
-        }
-        //往下走
-        if (maze[y+1][x]!=1 && maze[y+1][x]!=-1){
-       
-            route.add(new Point(x,y+1));
-            return true;
-        }
-        //往左走
-        if (maze[y][x-1]!=1 && maze[y][x-1]!=-1){
-       
-            route.add(new Point(x-1,y));
-            return true;
-        }
-        //往上走
-        if (maze[y-1][x]!=1 && maze[y-1][x]!=-1){
-       
-            route.add(new Point(x,y-1));
-            return true;
-        }
-        return false;
-    }
-    /*public static void findconnect(int [][]a)
+    void DFSTravel()
     {
-        for(int i=0;i<a.length;i++)
+        flag = new boolean[number];
+        for(int i=0;i<number;i++)
         {
-            for(int j=0;j<a[0].length;j++)
+            if(flag[i]==false)
             {
-                if(a[i][j]==1)
-                {
-                    System.out.println(i+": connect to "+j);
-                }
+                DFS(i);
             }
         }
-    }*/
+    }
+    void DFS(int i)
+    {
+        flag[i]=true;
+        System.out.print(vertexs[i]+" ");
+        for(int k=0;k<number;k++)
+        {
+            if(flag[k]=false&&edges[i][k]==1)
+            {
+                DFS(k);
+            }
+        }
+    }
+    public static void main (String [] args)
+    {
+        Graph bfs=new Graph();
+        System.out.println("DFS");
+        bfs.DFSTravel();
+    }
 }
